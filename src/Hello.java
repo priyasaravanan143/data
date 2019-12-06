0a48734dcc0116c22286dabfccb5f3b122518568
priyasaravanan143 <banupriyasaravanan1234@gmail.com>
2019-12-06 22:29:48 +0530

    code collection

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		String st="win";
+		StringBuilder dd=new StringBuilder(st);
+		StringBuilder ev=dd.reverse();
+		System.out.println(ev);
+	}
+
+}
